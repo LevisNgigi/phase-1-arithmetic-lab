ab92f81adc6093592bc6a18c64fcbd03ff98de31
Levis <Ngigilevis@gmail.com>
2023-12-14 11:22:29 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random());
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3%num4;
+
+let max = Math.max(2, 3, 4, 20, 5, 7, 8, 9, 10);
